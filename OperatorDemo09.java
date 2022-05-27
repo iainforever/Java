b1bfc1b8adef52a5a001c449aaf5301a21e370b2
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-27 14:45:16 +0800

    Create OperatorDemo09.java

---
+++ OperatorDemo09.java
@@ -0,0 +1,14 @@
+/*
+三元运算符
+ */
+public class OperatorDemo09 {
+    public static void main(String[] args){
+        //定义两个变量
+        int a= 10;
+        int b = 20;
+
+        //获取两个数据中的较大值
+        int max = a > b ? a : b;
+        System.out.println("max:"+max);
+    }
+}
