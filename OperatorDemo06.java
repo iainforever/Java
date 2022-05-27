c58c7775099e2c93a644c340711c0e34cb9bdba1
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-27 13:48:27 +0800

    Create OperatorDemo06.java

---
+++ OperatorDemo06.java
@@ -0,0 +1,31 @@
+/*
+关系运算符
+ */
+public class OperatorDemo06 {
+    public static void main(String[] args){
+        //定义变量
+        int i = 10;
+        int j = 20;
+        int k = 10;
+
+        //==
+        System.out.println(i == j);
+        System.out.println(i == k);
+        System.out.println("--------");
+
+        //!=
+        System.out.println(i != j);
+        System.out.println(i != k);
+        System.out.println("--------");
+
+        //>
+        System.out.println(i > j);
+        System.out.println(i > k);
+        System.out.println("--------");
+
+        //>=
+        System.out.println(i >= j);
+        System.out.println(i >= k);
+        System.out.println("--------");
+    }
+}
