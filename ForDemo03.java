38fb96fcbcb48cd07fd9a98193ecb3e4118485bd
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-28 21:31:20 +0800

    Create ForDemo03.java

---
+++ ForDemo03.java
@@ -0,0 +1,10 @@
+//求1~5的数据和
+public class ForDemo03 {
+    public static void main(String[] args){
+        int sum = 0;
+        for (int i = 1;i <= 5;i++){
+            sum +=  i;
+        }
+        System.out.println("sum:"+sum);
+    }
+}
