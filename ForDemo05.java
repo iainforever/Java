9fff2c006537ee69dceb33882d8e3f702e617b73
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-28 21:29:15 +0800

    Create ForDemo05.java

---
+++ ForDemo05.java
@@ -0,0 +1,13 @@
+//水仙花
+public class ForDemo05 {
+    public static void main(String[] args){
+        for(int i = 100;i <= 999;i++){
+            int a = i / 100;
+            int b = (i / 10) % 10;
+            int c= i % 10;
+            if(a * a * a + b * b *b + c * c * c == i){
+                System.out.println(i);
+            }
+        }
+    }
+}
