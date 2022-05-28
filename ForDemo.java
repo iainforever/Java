2ef66f2841434e89bdd975fec26248c3180ae98b
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-28 21:30:36 +0800

    Create ForDemo.java

---
+++ ForDemo.java
@@ -0,0 +1,8 @@
+//输出5次HelloWorld
+public class ForDemo {
+    public static void main(String[] args){
+        for(int i = 1;i <= 5;i++){
+            System.out.println("HelloWorld");
+        }
+    }
+}
