9d48c725f2757112154224f67c6f1895df4bb754
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-29 15:57:39 +0800

    Create Example22.java

---
+++ Example22.java
@@ -0,0 +1,9 @@
+/*数组遍历*/
+public class Example22 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        for(int i = 0;i <= 5;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
