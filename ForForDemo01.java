fe02389dcae2e9a0e8a4d77a06b539a68c24b88b
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-29 12:11:40 +0800

    Create ForForDemo01.java

---
+++ ForForDemo01.java
@@ -0,0 +1,11 @@
+public class ForForDemo01 {
+
+  public static void main(String[] args) {
+   
+   for(int h = 0;h <= 23;h++){
+     for(int m = 0;m <= 59;m++){
+       System.out.println("现在是北京时间："+h+"点"+m+"分");
+     }
+   }
+  }
+}
