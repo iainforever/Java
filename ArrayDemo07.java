7e957b91ec4cb27c599f7c3dccdc634966c62829
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-29 13:54:02 +0800

    Create ArrayDemo07.java

---
+++ ArrayDemo07.java
@@ -0,0 +1,13 @@
+public class ArrayDemo07 {
+    public static void main(String[] args) {
+        int[] arr = {12,45,98,73,60};
+        int max = arr[0];
+
+        for(int x = 1;x < arr.length;x++){
+            if(arr[x] > max){
+                max = arr[x];
+            }
+        }
+        System.out.println(max);
+    }
+}
