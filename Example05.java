fc0cf9521e2a1a287c6dde372248f1dcb7801456
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-29 14:20:03 +0800

    Create Example05.java

---
+++ Example05.java
@@ -0,0 +1,9 @@
+/*赋值类型中的强制转换*/
+public class Example05 {
+    public static void main(String[] args) {
+        short s = 3;
+        int x = 5;
+        s += x;
+        System.out.println("s="+s);
+    }
+}
