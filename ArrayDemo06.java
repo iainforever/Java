239015269747c110e3f1505096663403f84c4df6
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-29 13:45:27 +0800

    Create ArrayDemo06.java

---
+++ ArrayDemo06.java
@@ -0,0 +1,8 @@
+public class ArrayDemo06 {
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,6,3,7,3,6,3,6,3,};
+        for(int x = 0;x < arr.length;x++){
+            System.out.println(arr[x]);
+        }
+    }
+}
