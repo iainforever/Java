e7ff4caa8568a015c9b9ab34153d560e83aee014
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-29 15:00:25 +0800

    Create Example13.java

---
+++ Example13.java
@@ -0,0 +1,10 @@
+/*for循环语句，计算1~4的和*/
+public class Example13 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 1;i <= 4;i++){
+            sum += i;
+        }
+        System.out.println("sum="+sum);
+    }
+}
