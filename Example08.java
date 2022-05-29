a2087e6c25b32aef4918a1fef97340296412baec
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-29 14:33:20 +0800

    Create Example08.java

---
+++ Example08.java
@@ -0,0 +1,11 @@
+/*if...else语句*/
+public class Example08 {
+    public static void main(String[] args) {
+        int num = 19;
+        if(num % 2 == 0){
+            System.out.println(num+"是一个偶数。");
+        }else{
+            System.out.println(num+"是一个奇数。");
+        }
+    }
+}
