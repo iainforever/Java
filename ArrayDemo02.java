cc6a4806e254131720117f10dfbcf5145a9e7c84
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-29 13:06:16 +0800

    Create ArrayDemo02.java

---
+++ ArrayDemo02.java
@@ -0,0 +1,14 @@
+package com.calvin_01;
+/*数组动态初始化*/
+public class ArrayDemo02 {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+
+        //输出数组名
+     System.out.println(arr);//[I@2d98a335
+        // 输出数组中的元素;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+  }
+}
