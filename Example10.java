dca93a091253e0bde1802237182deab95026a020
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-29 14:47:05 +0800

    Create Example10.java

---
+++ Example10.java
@@ -0,0 +1,41 @@
+/*switch语句*/
+public class Example10 {
+    public static void main(String[] args) {
+        int album = 8;
+        switch (album){
+            case 1:
+                System.out.println("This is Taylor Swift era.");
+                break;
+            case 2:
+                System.out.println("This is Fearless era.");
+                break;
+            case 3:
+                System.out.println("This is Speak Now era.");
+                break;
+            case 4:
+                System.out.println("This is Red era.");
+                break;
+            case 5:
+                System.out.println("This is 1989 era.");
+                break;
+            case 6:
+                System.out.println("This is reputation era.");
+                break;
+            case 7:
+                System.out.println("This is Lover era.");
+                break;
+            case 8:
+                System.out.println("This is folklore era.");
+                break;
+            case 9:
+                System.out.println("This is evermore era.");
+                break;
+            case 10:
+                System.out.println("This is Fearless(Taylor's Version) era.");
+                break;
+            case 11:
+                System.out.println("This is Red(Taylor's Version) era.");
+                break;
+        }
+    }
+}
