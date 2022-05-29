0128b7873841a33889f40d2ed6c325c1bb4ecade
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-29 12:09:47 +0800

    Create ControlDemo01.java

---
+++ ControlDemo01.java
@@ -0,0 +1,14 @@
+public class ControlDemo01 {
+
+  public static void main(String[] args) {
+  
+  for(int i = 1;i <= 5;i++){
+    if(i % 2 == 0){
+      continue;
+      //break;
+    }
+    System.out.println(i);
+  }
+   
+  }
+}
