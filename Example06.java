f5f5d900aedf748c10d8d5467ad15ce295e1d8bc
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-29 14:26:24 +0800

    Create Example06.java

---
+++ Example06.java
@@ -0,0 +1,15 @@
+/*&和&&的区别*/
+public class Example06 {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        boolean a,b;
+        a = x >0 & y++ > 1;
+        System.out.println("a="+a);
+        System.out.println("y="+y);
+        b = x > 0 && z++ > 1;
+        System.out.println("b="+b);
+        System.out.println("z="+z);
+    }
+}
