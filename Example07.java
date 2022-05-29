990cea60b5c1d1a5789e790fea471aed10c51c74
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-29 14:29:52 +0800

    Create Example07.java

---
+++ Example07.java
@@ -0,0 +1,9 @@
+/*if语句*/
+public class Example07{
+    public static void main(String[] args) {
+        int age = 17;
+        if(age < 18){
+            System.out.println("此人是未成年人！");
+        }
+    }
+}
