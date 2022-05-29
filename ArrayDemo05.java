f04f1d89759ebc1efd5ecd804ab1be4cbf0dbf8e
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-29 13:36:01 +0800

    Create ArrayDemo05.java

---
+++ ArrayDemo05.java
@@ -0,0 +1,10 @@
+public class ArrayDemo05 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4};
+        System.out.println(arr);
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+    }
+}
