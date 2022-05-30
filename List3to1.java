3b0d4165ff27de3f9b8ea13032ff06ed81451a4b
iainforever <104896823+iainforever@users.noreply.github.com>
2022-05-30 22:21:11 +0800

    Create List3to1.java

---
+++ List3to1.java
@@ -0,0 +1,8 @@
+/**
+ * This is the first sample program in Core Java Chapter 3
+ */
+public class List3to1 {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello, World!'");
+    }
+}
